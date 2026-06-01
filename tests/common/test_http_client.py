36934da29233a9577078801137495c3e67a85da7
Shaun Fitzsimons <87677173+sfitz42@users.noreply.github.com>
2026-06-01 16:04:54 +0100

    Align to AICE standards + address security issues
    * Align to AICE standards
    
    * Update README and add Floci
    
    * Pin Mongo docker image
    
    * Update conftest.py

--- tests/common/test_http_client.py
+++ tests/common/test_http_client.py
@@ -1,7 +1,6 @@
 import httpx
 
-from app.common.http_client import hook_request_tracing
-from app.common.tracing import ctx_trace_id
+from app.common import http_client, tracing
 
 
 def mock_handler(request):
@@ -10,9 +9,9 @@ def mock_handler(request):
 
 
 def test_trace_id_missing():
-    ctx_trace_id.set("")
+    tracing.ctx_trace_id.set("")
     client = httpx.Client(
-        event_hooks={"request": [hook_request_tracing]},
+        event_hooks={"request": [http_client.hook_request_tracing]},
         transport=httpx.MockTransport(mock_handler),
     )
     resp = client.get("http://localhost:1234/test")
@@ -20,9 +19,9 @@ def test_trace_id_missing():
 
 
 def test_trace_id_set():
-    ctx_trace_id.set("trace-id-value")
+    tracing.ctx_trace_id.set("trace-id-value")
     client = httpx.Client(
-        event_hooks={"request": [hook_request_tracing]},
+        event_hooks={"request": [http_client.hook_request_tracing]},
         transport=httpx.MockTransport(mock_handler),
     )
     resp = client.get("http://localhost:1234/test")
--- tests/example/test_router.py
+++ tests/example/test_router.py
@@ -1,10 +1,9 @@
-from fastapi.testclient import TestClient
+import fastapi.testclient
 
-from app.common.http_client import create_async_client
-from app.common.mongo import get_db
-from app.main import app
+import app.entrypoints.fastapi
+from app.common import http_client, mongo
 
-client = TestClient(app)
+client = fastapi.testclient.TestClient(app.entrypoints.fastapi.app)
 
 
 def test_root_success():
@@ -18,7 +17,7 @@ def test_db_query_success(mocker):
     mock_db.example.insert_one.return_value = None
     mock_db.example.find_one.return_value = {"foo": "bar", "id": 123}
 
-    app.dependency_overrides[get_db] = lambda: mock_db
+    app.entrypoints.fastapi.app.dependency_overrides[mongo.get_db] = lambda: mock_db
 
     try:
         response = client.get("/example/db")
@@ -28,14 +27,16 @@ def test_db_query_success(mocker):
 
         mock_db.example.insert_one.assert_called_once()
     finally:
-        app.dependency_overrides = {}
+        app.entrypoints.fastapi.app.dependency_overrides = {}
 
 
 def test_http_query_success(mocker):
     mock_client = mocker.AsyncMock()
     mock_client.get.return_value.status_code = 200
 
-    app.dependency_overrides[create_async_client] = lambda: mock_client
+    app.entrypoints.fastapi.app.dependency_overrides[
+        http_client.create_async_client
+    ] = lambda: mock_client
 
     try:
         response = client.get("/example/http")
@@ -43,4 +44,4 @@ def test_http_query_success(mocker):
         assert response.status_code == 200
         assert response.json() == {"ok": 200}
     finally:
-        app.dependency_overrides = {}
+        app.entrypoints.fastapi.app.dependency_overrides = {}
