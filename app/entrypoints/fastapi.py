9b42fb202e5b25debb0ae2f8d0d1ce4aa430d1fc
Tim Gordon <tim.gordon@esynergy.co.uk>
2026-06-12 00:32:30 +0100

    Clarify FastAPI app name / purpose

--- app/entrypoints/fastapi.py
+++ app/entrypoints/fastapi.py
@@ -26,7 +26,7 @@ async def lifespan(_: fastapi.FastAPI) -> AsyncGenerator[None]:
         logger.info("MongoDB client closed")
 
 
-app = fastapi.FastAPI(lifespan=lifespan)
+app = fastapi.FastAPI(lifespan=lifespan, title="RPA Guidance Review")
 
 app.add_middleware(tracing.TraceIdMiddleware)
 
